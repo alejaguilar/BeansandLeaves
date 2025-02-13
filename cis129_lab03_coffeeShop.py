d6c59b3abf34462f6f018139c53827332d3edf5f
alejaguilar <aaguilar114@mail.pima.edu>
2025-02-12 18:55:50 -0700

    Update cis129_lab03_coffeeShop.py
    updated to include tea and scones as items for purchase, added to receipt

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -7,6 +7,8 @@
 
 PRICE_PER_COFFEE = 5.00
 PRICE_PER_MUFFIN = 4.00
+PRICE_PER_TEA = 3.00
+PRICE_PER_SCONE = 4.00
 TAX_RATE = 0.06
 
 #print greeting
@@ -20,15 +22,25 @@ numCoffee = int(input('Number of coffees bought?\n'))
 
 numMuffin = int(input('Number of muffins bought?\n'))
 
+#updated to include tea and scones as items for purchase
+
+numTea = int(input('Number of teas bought?\n'))
+
+numScone = int(input('Number of scones bought?\n'))
+
 # calculate total cost for each item type purchased
 
 cost_Coffees = numCoffee * PRICE_PER_COFFEE
 
 cost_Muffins = numMuffin * PRICE_PER_MUFFIN
 
+cost_Teas = numTea * PRICE_PER_TEA
+
+cost_Scones = numScone * PRICE_PER_SCONE
+
 # calculate subtotal and tax
 
-subTotal = cost_Coffees + cost_Muffins
+subTotal = cost_Coffees + cost_Muffins + cost_Teas + cost_Scones
 
 tax = subTotal * TAX_RATE
 
@@ -47,6 +59,10 @@ Beans and Leaves Eatery Receipt\n')
 print(numCoffee, 'Coffee at $', PRICE_PER_COFFEE, 'each: $', cost_Coffees,'\n')
 
 print(numMuffin, 'Muffins at $', PRICE_PER_MUFFIN, 'each: $', cost_Muffins,'\n')
+
+print(numTea, 'Coffee at $', PRICE_PER_TEA, 'each: $', cost_Teas,'\n')
+
+print(numScone, 'Coffee at $', PRICE_PER_SCONE, 'each: $', cost_Scones,'\n')
 
 print('6% tax: $', tax,'\n')
 
